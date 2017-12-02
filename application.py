2ee183e6ec91d0a66dba2b274cd275e52c7052cc
Cody Crow <cody@cms07.org>
2017-12-02 12:14:16 -0600

    Updated to have basic functionality once implementation is done

--- application.py
+++ application.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+
 from tkinter import *
 import tkinter.filedialog as fd
 import csv
-# import numpy
+# import pandas
 
 class Application(Frame):
+    filename = None
+    table_f = None
+
     def __init__(self, master=None):
         """
         Constructor for the UI class.
@@ -14,9 +19,35 @@ class Application(Frame):
 
     def create_widgets(self):
         self.load_but = Button(self, text='Load file', command=self.load_file)
-        self.load_but.pack(sid=TOP, padx=5, pady=5)
-        self.anlyz_but = Button(self, text='Analyze')  # add command to this button
+        self.load_but.pack(side=TOP, padx=5, pady=5)
+        self.anlyz_but = Button(self, text='Analyze', command = self.analyze_data)  # add command to this button
         self.anlyz_but.pack(side=TOP, padx=5, pady=5)
+
+    def analyze_data(self):
+        if not self.filename:
+            return
+
+        df = pandas.read_csv(self.filename)
+        # Pass into implementation
+        # predicted_df = implementation(df)
+
+        predicted_list = predicted_df.values.tolist()
+        self.create_table_from_list(predicted_list)
+
+
+    def create_table_from_list(self, data):
+        if self.table_f:
+            self.table_f.destroy()
+
+        nrow = len(data) if len(data) < 40 else 40
+        ncol = len(data[0])
+        self.table_f = Frame(self, bg='black')
+        self.table_f.pack(side=BOTTOM)
+        for row in range(nrow):
+            for col in range(ncol):
+                label = Label(self.table_f, text=data[row][col])
+                label.grid(row=row, column=col, sticky='nsew', padx=1, pady=1)
+
 
     def create_table(self, filename):
         data = list()
@@ -24,14 +55,8 @@ class Application(Frame):
             reader = csv.reader(csvfile)
             for row in reader:
                 data.append(row)
-        nrow = len(data)
-        ncol = len(data[0])
-        table_f = Frame(self, bg='black')
-        table_f.pack(side=BOTTOM)
-        for row in range(nrow):
-            for col in range(ncol):
-                label = Label(table_f, text=data[row][col])
-                label.grid(row=row, column=col, sticky='nsew', padx=1, pady=1)
+        self.create_table_from_list(data)
+
 
     def load_file(self):
         """
@@ -43,6 +68,7 @@ class Application(Frame):
             # do stuff with the file.
             # open it as csv or as numpy object.
             print('File picked is : {}'.format(filename))
+            self.filename = filename
             self.create_table(filename)
         else:
             pass
@@ -55,7 +81,8 @@ def main():
     """
     root = Tk()
     root.title('CS 5593 Project')
-    root.geometry('500x500')
+    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+    root.geometry("%dx%d+0+0" % (w, h))
 
     # create the UI to display
     ui = Application(master=root)
