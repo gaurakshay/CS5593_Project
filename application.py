c68b4ec5d9b8a2abd1ef7c6565803c9ea7b771a3
Akshay Gaur <gaur.akshay@gmail.com>
2017-12-04 01:23:31 -0600

    Updated UI.

--- application.py
+++ application.py
@@ -1,37 +1,105 @@
 from tkinter import *
 import tkinter.filedialog as fd
 import csv
+import time
 # import numpy
+# import pandas
+
 
 class Application(Frame):
     def __init__(self, master=None):
         """
         Constructor for the UI class.
         """
+        self.root = master
         super().__init__(master)
-        self.pack()
+        self.pack(fill=BOTH, expand=True, padx=10, pady=10)
+        self.load_but = None
+        self.analyze_but = None
+        self.data = None
+        self.table_f = None
+        self.status = None
+        self.header_ch = None
+        self.header = None
+        self.res_lab = None
         self.create_widgets()
 
     def create_widgets(self):
-        self.load_but = Button(self, text='Load file', command=self.load_file)
-        self.load_but.pack(sid=TOP, padx=5, pady=5)
-        self.anlyz_but = Button(self, text='Analyze')  # add command to this button
-        self.anlyz_but.pack(side=TOP, padx=5, pady=5)
+        self.status = Label(self.root, bd=1, text='Pick a file to start.', relief=SUNKEN, anchor=W)
+        self.status.pack(side=BOTTOM, fill=X)
+        bot_frame = Frame(self)
+        bot_frame.pack(side=BOTTOM, fill=X)
+        self.analyze_but = Button(bot_frame, text='Analyze', state='disabled', command=self.analyze_data)  # add command to this button
+        self.analyze_but.pack(side=RIGHT, padx=5)
+        self.load_but = Button(bot_frame, text='Load file', command=self.load_file)
+        self.load_but.pack(side=RIGHT, padx=5)
 
-    def create_table(self, filename):
-        data = list()
+    def analyze_data(self):
+        self.update_status('Analyzing.')
+        Tk.update(self.root)
+        # check if user data contains headers.
+        if self.header.get():
+            # use data excluding first row.
+            pass
+        else:
+            # use all of the data.
+            pass
+        # here we can call the algorithm.
+        final_result = self.test()
+        self.update_status('Analysis complete.')
+        self.del_table()
+        self.show_results(final_result)
+
+    def show_results(self, results):
+        self.res_lab = Label(self, text=results)
+        self.res_lab.pack(side=TOP)
+
+    def test(self):
+        time.sleep(2)
+        return 'THE BITCOIN IS STABLE!!!'
+
+    def update_status(self, status_):
+        self.status.config(text=status_)
+
+    def load_data(self, filename):
+        self.data = list()
         with open(filename, 'r') as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
-                data.append(row)
+                self.data.append(row)
+        self.update_status('Data loaded successfully.')
+
+    def del_table(self):
+        self.table_f.destroy()
+        self.header = None
+        self.header_ch.destroy()
+        self.table_f.pack_forget()
+        self.header_ch.pack_forget()
+
+    def create_table(self, num_rows=10, data=None):
+        """
+        Create the table that shows snapshot of the CSV
+        picked.
+        :param num_rows:
+        :return:
+        """
+        if self.res_lab:
+            self.res_lab.destroy()
+            self.res_lab.pack_forget()
+        if data is None:
+            data = self.data
         nrow = len(data)
         ncol = len(data[0])
-        table_f = Frame(self, bg='black')
-        table_f.pack(side=BOTTOM)
-        for row in range(nrow):
+        self.table_f = Frame(self, bg='black')
+        self.table_f.pack(side=TOP)
+        for row in range(min(num_rows, nrow)):
             for col in range(ncol):
-                label = Label(table_f, text=data[row][col])
+                label = Label(self.table_f, text=data[row][col])
                 label.grid(row=row, column=col, sticky='nsew', padx=1, pady=1)
+        self.header = IntVar()
+        self.header_ch = Checkbutton(self, text='Data contains header.', variable=self.header)
+        self.header_ch.pack(side=TOP)
+        self.update_status('Data displayed.')
 
     def load_file(self):
         """
@@ -43,14 +111,23 @@ class Application(Frame):
             # do stuff with the file.
             # open it as csv or as numpy object.
             print('File picked is : {}'.format(filename))
-            self.create_table(filename)
+            self.update_status('File selected.')
+
+            # Load the data.
+            self.load_data(filename)
+
+            # Show data as table.
+            self.create_table()
+
+            # Enable analyze button.
+            self.analyze_but.config(state='normal')
         else:
             pass
 
 
 def main():
     """
-    This will be called when te program starts.
+    This will be called when the program starts.
     :return:
     """
     root = Tk()
@@ -58,7 +135,7 @@ def main():
     root.geometry('500x500')
 
     # create the UI to display
-    ui = Application(master=root)
+    Application(master=root)
 
     # keep the UI running
     root.mainloop()
